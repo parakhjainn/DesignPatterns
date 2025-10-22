0000274fbc9c308ebfcab3a82d21865357b1d7c6
Parakh Jain <parakhjainwork@gmail.com>
2025-10-22 15:45:24 +0530

    Device Manager Added

---
+++ Spotify/managers/DeviceManager.hpp
@@ -0,0 +1,52 @@
+#pragma once
+
+#include<iostream>
+#include "../device/IAudioOutputDevice.hpp"
+#include "../enums/DeviceType.hpp"
+#include "../factories/DeviceFactory.hpp"
+
+using namespace std;
+
+class DeviceManager {
+    IAudioOutputDevice* currentOutputDevice;
+
+    DeviceManager() {
+        currentOutputDevice = nullptr;
+    }
+
+public:
+    static DeviceManager& getInstance() {
+        static DeviceManager instance;
+        return instance;
+    }
+    
+    void connect(DeviceType deviceType) {
+        if (currentOutputDevice) {
+            delete currentOutputDevice;
+        }
+
+        currentOutputDevice = DeviceFactory::createDevice(deviceType);
+
+        switch(deviceType) {
+            case DeviceType::BLUETOOTH:
+                cout<< "Bluetooth device connected \n";
+                break;
+            case DeviceType::WIRED:
+                cout<< "Wired device connected \n";
+                break;
+            case DeviceType::HEADPHONES:
+                cout<< "Headphones connected \n";
+        }
+    }
+
+    IAudioOutputDevice* getOutputDevice() {
+        if (!currentOutputDevice) {
+            throw runtime_error("No output device is connected.");
+        }
+        return currentOutputDevice;
+    }
+
+    bool hasOutputDevice() {
+        return currentOutputDevice != nullptr;
+    }
+};
